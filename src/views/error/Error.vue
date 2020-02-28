31a6c1a87cc2a7af694e4036bd8243cdc103c942
Lucas De Almeida Paula <lapaula1@stefanini.com>
2020-02-28 17:02:25 -0300

    Design tela de erro

--- src/views/error/Error.vue
+++ src/views/error/Error.vue
@@ -1,22 +1,15 @@
 <template src="./Error.html"></template>
 <script lang="ts">
-
-import Vue from 'vue';
-import { Component, Prop } from 'vue-property-decorator';
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
 
 @Component({
-  components: {
-  },
+  components: {}
 })
-
 export default class ErrorComponent extends Vue {
-
   constructor() {
     super();
   }
-
-  
 }
-
 </script>
 <style lang="scss" src="./Error.scss"></style>
